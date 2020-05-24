700f51bef1989d04cf449917bf6a340ce83de9cc
Lancaster Wu <wuj@allegheny.edu>
2020-05-23 20:25:41 -0400

    finished pipeline iterative

---
+++ src/pipeline/fail_iterative_update_result.py
@@ -0,0 +1,22 @@
+"""Sample file for correct implementation"""
+
+def compute_iterative_factorial(value):
+    """Assumes value is a natural number
+    Returns value!"""
+    if value < 0:
+        raise ValueError("Inputs of 0 or grater!")
+        value = 0
+    result = 1
+    while value != 0:
+        result *= (value + 1) # fault
+        value -= 1
+    return result
+
+
+def compute_recursive_factorial(value):
+    """Assumes value is a natural number
+    Returns value!"""
+    if value < 1:
+        return 1
+    else:
+        return value * compute_recursive_factorial(value - 1)
---
+++ src/pipeline/fail_iterative_wrong_return.py
@@ -0,0 +1,22 @@
+"""Sample file for correct implementation"""
+
+def compute_iterative_factorial(value):
+    """Assumes value is a natural number
+    Returns value!"""
+    if value < 0:
+        raise ValueError("Inputs of 0 or grater!")
+        value = 0
+    result = 1
+    while value != 0:
+        result *= value
+        value -= 1
+    return result - 1  # fault
+
+
+def compute_recursive_factorial(value):
+    """Assumes value is a natural number
+    Returns value!"""
+    if value < 1:
+        return 1
+    else:
+        return value * compute_recursive_factorial(value - 1)
